e70767c20b33a14655c49d681e86fde1d03b31fa
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-13 11:39:52 +0300

    feat(entity): add `Location`

---
+++ src/main/java/org/as1iva/entity/Location.java
@@ -0,0 +1,33 @@
+package org.as1iva.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @Column(nullable = false)
+    private BigDecimal latitude;
+
+    @Column(nullable = false)
+    private BigDecimal longitude;
+}
