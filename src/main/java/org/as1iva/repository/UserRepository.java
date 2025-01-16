07b2e39637fc802bd9aa623e60243750baf032f8
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-16 22:46:21 +0300

    feat(repository): add `findByLogin` method in `UserRepository`

--- src/main/java/org/as1iva/repository/UserRepository.java
+++ src/main/java/org/as1iva/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package org.as1iva.repository;
 
 import org.as1iva.entity.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public class UserRepository extends BaseRepository<Long, User>{
@@ -11,5 +14,13 @@ public class UserRepository extends BaseRepository<Long, User>{
     @Autowired
     public UserRepository(SessionFactory sessionFactory) {
         super(User.class, sessionFactory);
+    }
+
+    public Optional<User> findByLogin(String login) {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("FROM User u WHERE u.login = :login", User.class)
+                .setParameter("login", login)
+                .uniqueResultOptional();
     }
 }
