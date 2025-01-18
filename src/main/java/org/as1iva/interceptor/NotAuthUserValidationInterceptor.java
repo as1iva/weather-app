64df373f130ff4cacb588f8157f6f1aa3406e682
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-18 13:14:10 +0300

    feat(interceptor): add `NotAuthUserValidationInterceptor`

---
+++ src/main/java/org/as1iva/interceptor/NotAuthUserValidationInterceptor.java
@@ -0,0 +1,37 @@
+package org.as1iva.interceptor;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.as1iva.service.AuthService;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class NotAuthUserValidationInterceptor implements HandlerInterceptor {
+
+    private final AuthService authService;
+
+    @Override
+    public boolean preHandle(@NonNull HttpServletRequest req,
+                             @NonNull HttpServletResponse resp,
+                             @NonNull Object handler) throws Exception {
+
+        Cookie cookie = authService.getCookie(req);
+
+        if (isSessionNotValid(cookie)) {
+            resp.sendRedirect("/login");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isSessionNotValid(Cookie cookie) {
+        return (cookie == null || cookie.getValue() == null || !authService.getSession(cookie.getValue()).isPresent());
+    }
+}
