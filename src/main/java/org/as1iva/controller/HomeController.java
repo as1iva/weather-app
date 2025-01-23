8f1ecb6522d9887fbf87dd40b7d17fc889c8bafe
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-23 12:27:33 +0300

    refactor(controller): replace `WebUtils.getCookie()` with `@CookieValue` for best practice

--- src/main/java/org/as1iva/controller/HomeController.java
+++ src/main/java/org/as1iva/controller/HomeController.java
@@ -1,14 +1,12 @@
 package org.as1iva.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.as1iva.entity.Session;
 import org.as1iva.service.AuthService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.util.WebUtils;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,10 +15,10 @@ public class HomeController {
     private final AuthService authService;
 
     @GetMapping("/")
-    public String home(Model model, HttpServletRequest req) {
+    public String home(@CookieValue(name = "sessionId", required = false) String sessionId,
+                       Model model) {
 
-        Cookie cookie = WebUtils.getCookie(req, "sessionId");
-        Session session = authService.getSession(cookie.getValue()).get();
+        Session session = authService.getSession(sessionId).get();
 
         model.addAttribute("username", session.getUserId().getLogin());
 
