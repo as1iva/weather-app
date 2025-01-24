850f93fbf60ea8fa48e76f435abfa2cdd34a3b99
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-24 13:10:20 +0300

    refactor(dto): clean up redundant imports in `LocationApiResponseDto`

--- src/main/java/org/as1iva/dto/LocationApiResponseDto.java
+++ src/main/java/org/as1iva/dto/LocationApiResponseDto.java
@@ -2,9 +2,7 @@ package org.as1iva.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
