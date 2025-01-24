967295a389b0f34b01389e398b13ce926810129a
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-24 10:40:53 +0300

    feat(repository): add `findAllByUserId` method in `LocationRepository`

--- src/main/java/org/as1iva/repository/LocationRepository.java
+++ src/main/java/org/as1iva/repository/LocationRepository.java
@@ -1,9 +1,13 @@
 package org.as1iva.repository;
 
 import org.as1iva.entity.Location;
+import org.as1iva.entity.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class LocationRepository extends BaseRepository<Long, Location> {
@@ -11,5 +15,13 @@ public class LocationRepository extends BaseRepository<Long, Location> {
     @Autowired
     public LocationRepository(SessionFactory sessionFactory) {
         super(Location.class, sessionFactory);
+    }
+
+    public List<Location> findAllByUserId(User userId) {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("FROM Location l WHERE l.userId = :userId", Location.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 }
