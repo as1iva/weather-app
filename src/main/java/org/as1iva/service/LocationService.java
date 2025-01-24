2608204538893771800ebf33d91edbc56c064087
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-24 10:41:30 +0300

    feat(service): add `getAllByUserId` method in `LocationService`

--- src/main/java/org/as1iva/service/LocationService.java
+++ src/main/java/org/as1iva/service/LocationService.java
@@ -2,10 +2,15 @@ package org.as1iva.service;
 
 import lombok.RequiredArgsConstructor;
 import org.as1iva.dto.LocationRequestDto;
+import org.as1iva.dto.LocationResponseDto;
 import org.as1iva.entity.Location;
+import org.as1iva.entity.User;
 import org.as1iva.repository.LocationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -23,5 +28,23 @@ public class LocationService {
                 .build();
 
         locationRepository.save(location);
+    }
+
+    public List<LocationResponseDto> getAllByUserId(User userId) {
+        List<LocationResponseDto> locationResponseDtos = new ArrayList<>();
+
+        List<Location> locations = locationRepository.findAllByUserId(userId);
+
+        for (Location location : locations) {
+            locationResponseDtos.add(LocationResponseDto.builder()
+                    .id(location.getId())
+                    .name(location.getName())
+                    .userId(location.getUserId())
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
+                    .build());
+        }
+
+        return locationResponseDtos;
     }
 }
