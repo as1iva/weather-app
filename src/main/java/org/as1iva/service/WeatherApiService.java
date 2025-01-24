b244217acac524c50ee33e9a9f512f5f40b50cc3
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-24 13:08:58 +0300

    feat(service): add `getWeatherByCoordinates` method in `WeatherApiService`

--- src/main/java/org/as1iva/service/WeatherApiService.java
+++ src/main/java/org/as1iva/service/WeatherApiService.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.as1iva.dto.LocationApiResponseDto;
+import org.as1iva.dto.LocationResponseDto;
+import org.as1iva.dto.WeatherApiResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,5 +39,21 @@ public class WeatherApiService {
                 .block();
 
         return objectMapper.readValue(jsonResponse, new TypeReference<List<LocationApiResponseDto>>() {});
+    }
+
+    public WeatherApiResponseDto getWeatherByCoordinates(LocationResponseDto location) throws JsonProcessingException {
+        String jsonResponse = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/data/2.5/weather")
+                        .queryParam("lat", location.getLatitude())
+                        .queryParam("lon", location.getLongitude())
+                        .queryParam("units", "metric")
+                        .queryParam("appId", apiKey)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return objectMapper.readValue(jsonResponse, WeatherApiResponseDto.class);
     }
 }
