a148013505f9a0db7c09faa484a116f5d2302e10
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-24 13:14:48 +0300

    feat(controller): add weather display logic to `HomeController`

--- src/main/java/org/as1iva/controller/HomeController.java
+++ src/main/java/org/as1iva/controller/HomeController.java
@@ -1,12 +1,20 @@
 package org.as1iva.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.as1iva.dto.LocationResponseDto;
+import org.as1iva.dto.WeatherApiResponseDto;
 import org.as1iva.entity.Session;
 import org.as1iva.service.AuthService;
+import org.as1iva.service.LocationService;
+import org.as1iva.service.WeatherApiService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,13 +24,28 @@ public class HomeController {
 
     private final AuthService authService;
 
+    private final LocationService locationService;
+
+    private final WeatherApiService weatherApiService;
+
     @GetMapping("/")
     public String home(@CookieValue(name = "sessionId", required = false) String sessionId,
-                       Model model) {
+                       Model model) throws JsonProcessingException {
 
         Session session = authService.getSession(sessionId).get();
-
         model.addAttribute("username", session.getUserId().getLogin());
+
+        List<LocationResponseDto> locations = locationService.getAllByUserId(session.getUserId());
+
+        List<WeatherApiResponseDto> weatherApiResponseDtos = new ArrayList<>();
+
+        for (LocationResponseDto location : locations) {
+            WeatherApiResponseDto weather = weatherApiService.getWeatherByCoordinates(location);
+
+            weatherApiResponseDtos.add(weather);
+        }
+
+        model.addAttribute(weatherApiResponseDtos);
 
         return INDEX;
     }
