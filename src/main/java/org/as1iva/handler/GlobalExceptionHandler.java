524efff93a4c4c0198b6e0b0752c9022f95e8368
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-27 16:22:40 +0300

    feat(handler): add `GlobalExceptionHandler`

---
+++ src/main/java/org/as1iva/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package org.as1iva.handler;
+
+import org.as1iva.exception.UserAuthenticationFailedException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final String SIGN_IN = "sign-in";
+
+    @ExceptionHandler(UserAuthenticationFailedException.class)
+    public String handle(UserAuthenticationFailedException e, Model model) {
+        model.addAttribute("messageError", e.getMessage());
+        return SIGN_IN;
+    }
+}
