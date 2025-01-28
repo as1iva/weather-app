32911480afd5392ff3cf06f40f175de455f0df93
Tim Fadeev <1mailforwork1@gmail.com>
2025-01-28 13:06:06 +0300

    feat(dto): add `UserRegistrationRequestDto`

---
+++ src/main/java/org/as1iva/dto/UserRegistrationRequestDto.java
@@ -0,0 +1,25 @@
+package org.as1iva.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationRequestDto {
+
+    @NotBlank(message = "Login cannot be empty. Please enter a valid login")
+    @Size(min = 5, max = 10, message = "Length must be between 3 and 10")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$", message = "Login must contain both English letters and numbers")
+    private String login;
+
+    @NotBlank(message = "Password cannot be empty. Please enter a valid password")
+    @Size(min = 5, message = "Length must be at least 8")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$", message = "Password must contain both English letters and numbers")
+    private String password;
+
+    private String repeatPassword;
+}
